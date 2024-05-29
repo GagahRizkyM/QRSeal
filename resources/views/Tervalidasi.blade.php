a5c7524e1eba2ad38e73aa49e0d2517e90de6f27
GagahRizkyM <72647884+GagahRizkyM@users.noreply.github.com>
2024-05-29 14:42:32 +0700

    Create Tervalidasi.blade.php

---
+++ resources/views/Tervalidasi.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Sertifikat Tervalidasi Asli</title>
+  <style>
+    body {
+      font-family: sans-serif;
+      margin: 0;
+      padding: 0;
+      background-color: white;
+    }
+    .header {
+      background-color: #00b0ff;
+      color: white;
+      padding: 10px;
+      text-align: right;
+    }
+    .content {
+      padding: 20px;
+    }
+    .check-mark {
+      display: block;
+      margin: 0 auto;
+      width: 100px;
+      height: 100px;
+      border-radius: 50%;
+      background-color: #4CAF50;
+      margin-bottom: 20px;
+    }
+    .check-mark::before {
+      content: '';
+      position: absolute;
+      top: 35px;
+      left: 35px;
+      width: 30px;
+      height: 60px;
+      border-right: 10px solid white;
+      border-top: 10px solid white;
+      transform: rotate(45deg);
+    }
+    .title {
+      font-size: 24px;
+      font-weight: bold;
+      margin-bottom: 10px;
+    }
+    .details {
+      text-align: center;
+    }
+  </style>
+</head>
+<body>
+  <div class="header">
+    <a href="#">QRSeal</a>
+    <a href="#">Logout</a>
+  </div>
+  <div class="content">
+    <div class="check-mark"></div>
+    <h1 class="title">Sertifikat Tervalidasi Asli</h1>
+    <div class="details">
+      Disahkan oleh Amanda Brophy<br>
+      Pada tanggal 15 Juli 2022
+    </div>
+  </div>
+</body>
+</html>

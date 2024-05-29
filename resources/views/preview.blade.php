feaa60549f6c85265d178684bbaf109cdf9cc123
GagahRizkyM <72647884+GagahRizkyM@users.noreply.github.com>
2024-05-29 14:27:28 +0700

    Update preview.blade.php

--- resources/views/preview.blade.php
+++ resources/views/preview.blade.php
@@ -3,7 +3,7 @@
 
 <div class="text-center">
     <h1> Preview </h1>
-     <button class="btn btn-primary" style="background-color: #40A2D8;" type="submit">Generate QR-CODE</button>
+     <button class="btn btn-primary" style="background-color: #40A2D8;" type="submit">Proses</button>
   </div>
 
 
